12f38dc317b4707abf1779b18868790e4ab87d9c
Oliver <57992834+MrOliverLang@users.noreply.github.com>
2021-08-03 19:47:50 +0200

    product screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,7 +6,7 @@ export default function Product(props) {
   return (
     <div key={product._id} className="card">
       <a href={`/product/${product._id}`}>
-        <img className="medium" src={product.iamge} alt="product" />
+        <img className="medium" src={product.image} alt="product" />
       </a>
       <div className="card-body">
         <a href={`/product/${product._id}`}>
