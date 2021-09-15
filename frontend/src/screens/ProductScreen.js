ed25bf67a1b4eb2d888981d009763e85a2955708
Oliver <57992834+MrOliverLang@users.noreply.github.com>
2021-09-15 13:34:22 +0200

    load products from backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Rating from "../components/Rating";
 import data from "../data";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 export default function ProductScreen(props) {
   const product = data.products.find((x) => x._id === props.match.params.id);
   if (!product) {
@@ -9,7 +9,7 @@ export default function ProductScreen(props) {
   }
   return (
     <div>
-        <Link to="/">Back to result</Link>
+      <Link to="/">Back to result</Link>
       <div className="row top">
         <div className="col-2">
           <img className="large" src={product.image} alt={product.name}></img>
@@ -47,7 +47,7 @@ export default function ProductScreen(props) {
                     {product.countInStock > 0 ? (
                       <span className="success">In Stock</span>
                     ) : (
-                      <span className="error">Unavailable</span>
+                      <span className="danger">Unavailable</span>
                     )}
                   </div>
                 </div>
